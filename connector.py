f9f0b265269ec50669ebb4a23b51d65a01bbb5a0
bds18b <81664374+bds18b@users.noreply.github.com>
2022-03-04 19:35:53 -0500

    Update connector.py

--- connector.py
+++ connector.py
@@ -1,6 +1,9 @@
 import mysql.connector
+# Database management program (mostly for testings and makes removing tables and the db itself easy)
+# The prompt function is at the very bottom of this file
 
-# connect to sql in order to create the db
+
+# connect to sql server in order to create the db (need to have mysql server installed)
 conn = mysql.connector.connect(
     host='localhost',
     # the user should default to root when you install it
@@ -9,24 +12,101 @@ conn = mysql.connector.connect(
     password='testing'
 )
 
-# create the db
-conn_cursor = conn.cursor()
-conn_cursor.execute("CREATE DATABASE shortstory_db")
+def create_db():
+    try:
+        # create the db
+        conn_cursor = conn.cursor()
+        conn_cursor.execute("CREATE DATABASE shortstory_db")
+        print("Database created successfully!")
 
-# connect to the new db
-db = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    password='testing',
-    database='shortstory_db'
-)
+    except:
+        print("Database already exists!")
 
-cursor = db.cursor()
-# create a 'users' table with the primary key as userID
-cursor.execute('CREATE TABLE users (userID INT NOT NULL AUTO_INCREMENT, username VARCHAR(10), password VARCHAR(10), PRIMARY KEY (userID))')
-print("Table created successfully")
-db.close()
-conn.close()
+def create_tables():
+    # connect to the db
+    try:
+        db = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='testing',
+            database='shortstory_db'
+        )
+    except:
+        print("Database does not exist!")
+        return
+
+    cursor = db.cursor()
+    # create the tables
+    try:
+        cursor.execute(
+            'CREATE TABLE users (username VARCHAR(20) NOT NULL UNIQUE, password VARCHAR(20) NOT NULL, PRIMARY KEY (username))')
+        cursor.execute(
+            'CREATE TABLE storage (prompt VARCHAR(25) NOT NULL UNIQUE, genre VARCHAR(10) NOT NULL, PRIMARY KEY (prompt))')
+        cursor.execute(
+            'CREATE TABLE stories (story_name VARCHAR(25) NOT NULL UNIQUE, prompt VARCHAR(25) NOT NULL UNIQUE, author1 VARCHAR(10) NOT NULL, author2 VARCHAR(10) NOT NULL, PRIMARY KEY (story_name), FOREIGN KEY (prompt) REFERENCES storage(prompt))')
+        print("Tables created successfully!")
+        db.close()
+    except:
+        print("Tables already exist!")
+        db.close()
+
+def drop_db():
+    try:
+        # drop the db
+        conn_cursor = conn.cursor()
+        conn_cursor.execute("DROP DATABASE shortstory_db")
+        print("Database dropped successfully!")
+
+    except:
+        print("Database doesn't exist!")
+
+
+def drop_tables():
+    # connect to the db
+    try:
+        db = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='testing',
+            database='shortstory_db'
+        )
+    except:
+        print("Database does not exist!")
+        return
+
+    cursor = db.cursor()
+    # drop the tables
+    try:
+        cursor.execute('DROP TABLE users')
+        cursor.execute('DROP TABLE stories')
+        cursor.execute('DROP TABLE storage')
+        print("Tables dropped successfully!")
+        db.close()
+    except:
+        print("Table already exists!")
+        db.close()
+
+while True:
+    choice = (input("Choose from the below options:\n"
+                    "1. Create the database\n"
+                    "2. Create the tables\n"
+                    "3. Drop the database\n"
+                    "4. Drop the tables\n"
+                    "5. Exit\n"))
+    if choice == str(1):
+        create_db()
+    elif choice == str(2):
+        create_tables()
+    elif choice == str(3):
+        drop_db()
+    elif choice == str(4):
+        drop_tables()
+    elif choice == str(5):
+        quit()
+    else:
+        print("Invalid input")
+
+
 
 
 
